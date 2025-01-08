152b6d184e64e8cb35498b801e8ba585c150685d
Serhii Babanov <serhii.babanov@gmail.com>
2025-01-07 21:41:19 -0500

    add images for products

---
+++ dao/src/main/resources/db/changelog/devLogs/insert-images.sql
@@ -0,0 +1,20 @@
+-- liquibase formatted sql
+insert into images (id, product_id, link, image_order)
+values
+    (1, 1, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (2, 1, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/suodhm3fll3uafiutwqb.avif', 2),
+    (3, 1, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/xlzic1et5k2dcswp511k.avif', 3),
+    (4, 1, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303496/onlinestore/aalgi4a27q7p1ozp1x5o.avif', 4),
+    (5, 2, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (6, 3, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (7, 4, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (8, 5, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (9, 6, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (10, 7, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (11, 8, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (12, 9, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (13, 10, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (14, 11, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (15, 12, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (16, 13, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1),
+    (17, 14, 'https://res.cloudinary.com/dpnzbwijt/image/upload/v1736303497/onlinestore/gfe1hm2b0to2zr0ksjtv.avif', 1);
